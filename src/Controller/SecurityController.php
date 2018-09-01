bb6d9d4d36a3518eb88ada0e4c604aa5dc4f4b23
botflux <victor.mendele68@gmail.com>
2018-09-01 17:33:56 +0200

    Add simple authentification

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login")
+     * @param AuthenticationUtils $utils
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function login(AuthenticationUtils $utils, AuthorizationCheckerInterface $checker)
+    {
+        if (true === $checker->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirectToRoute('dashboard');
+        }
+
+
+        $error = $utils->getLastAuthenticationError();
+        $lastUsername = $utils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'error' => $error,
+            'last_username' => $lastUsername
+        ]);
+    }
+}
