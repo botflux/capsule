2456049dac43b5356539aef7f3b57d04b7ddae85
botflux <victor.mendele68@gmail.com>
2018-09-08 17:49:55 +0200

    Add vue material

--- assets/js/App.vue
+++ assets/js/App.vue
@@ -1,16 +1,52 @@
 <template>
-    <div id="app">
-        Hello world
-        <router-view></router-view>
-    </div>
+    <md-app md-mode="reveal">
+        <md-app-toolbar class="md-primary">
+            <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
+                <md-icon>menu</md-icon>
+            </md-button>
+            <span class="md-title">My Title</span>
+        </md-app-toolbar>
+
+        <md-app-drawer :md-active.sync="menuVisible">
+            <md-toolbar class="md-transparent" md-elevation="0">Navigation</md-toolbar>
+
+            <md-list>
+                <md-list-item to="/" @click="hideDrawer">
+                    <md-icon>home</md-icon>
+                    <span class="md-list-item-text">Home</span>
+                </md-list-item>
+
+                <md-list-item to="/account" @click="hideDrawer">
+                    <md-icon>settings</md-icon>
+                    <span class="md-list-item-text">Account</span>
+                </md-list-item>
+            </md-list>
+        </md-app-drawer>
+
+        <md-app-content>
+
+            Hello world
+            <router-view></router-view>
+        </md-app-content>
+    </md-app>
 </template>
 
 <script>
   export default {
-    name: "App"
+    name: 'App',
+    data: () => ({
+      menuVisible: false
+    }),
+    methods: {
+      hideDrawer () {
+        this.menuVisible = false
+      }
+    }
   }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+    .md-app {
+        min-height: 100vh;
+    }
 </style>
